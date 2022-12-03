6be9ff89ae00fe8b61b7ac2ac3ad3a524fb1cf34
Nez <nezzengame@gmail.com>
2022-12-03 21:07:41 +0100

    Main dla projektu warsztat

---
+++ src/main/java/pl/sda/j133/hibernate/warsztat/Main.java
@@ -0,0 +1,37 @@
+package pl.sda.j133.hibernate.warsztat;
+
+import pl.sda.j133.hibernate.warsztat.komendy.*;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Komenda> listaKomend = List.of(
+                new KomendaDodajPojazd(),
+                new KomendaDodajMechanik(),
+                new KomendaDodajSerwisPojazdu(),
+
+                new KomendaListaPojazd(),
+                new KomendaListaMechanik(),
+                new KomendaListaSerwisPojazdu(),
+
+                new KomendaZnajdzPojazd(),
+                new KomendaZnajdzMechanik(),
+                new KomendaZnajdzSerwisPojazdu(),
+
+                new KomendaUsunPojazd(),
+                new KomendaUsunMechanik(),
+                new KomendaUsunSerwisPojazdu()
+        );
+        System.out.println("Lista dostepnych komend:");
+        listaKomend.forEach(komenda -> System.out.println(komenda.getKomenda()));
+
+        System.out.println("Podaj komende");
+        String komenda = Komenda.scanner.nextLine();
+
+        listaKomend
+                .stream()
+                .filter(dostepnaKomenda -> dostepnaKomenda.getKomenda().equalsIgnoreCase(komenda))
+                .findFirst().ifPresent(Komenda::obsluga);
+        }
+}
